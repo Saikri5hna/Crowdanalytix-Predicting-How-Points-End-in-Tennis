cc631bef686d6a7bd8f84f6dd3397cb4bc94a435
SK <dsai.500@gmail.com>
2018-01-16 23:40:54 +0530

    1.Starting up

---
+++ code1.R
@@ -0,0 +1,11 @@
+rm(list = ls())
+setwd("C:/Users/Sai Krishna/Documents/Data Science competitions/Crowdanalytix-Predicting How Points End in Tennis/Raw Files/")
+
+list.files()
+
+mens_train <- read.csv("mens_train_file.csv" )
+mens_test <- read.csv("mens_test_file.csv" )
+womens_train <- read.csv("womens_train_file.csv" )
+womens_test <- read.csv("womens_test_file.csv" )
+
+str(mens_train)
